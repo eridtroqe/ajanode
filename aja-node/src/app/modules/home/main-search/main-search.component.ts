ad88805192b4feab704288f704ab819e3d0242c0
eridtroqe <erid.troqe@gmail.com>
2020-04-13 20:22:34 +0200

    restructured project

---
+++ aja-node/src/app/modules/home/main-search/main-search.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-search',
+  templateUrl: './main-search.component.html',
+  styleUrls: ['./main-search.component.scss']
+})
+export class MainSearchComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
