8533c9eeb8e279e67d3ee1f0cb051cd2e956e245
eridtroqe <erid.troqe@gmail.com>
2020-04-19 18:34:03 +0200

    added card-real-real-estate.component

---
+++ aja-node/src/app/modules/real-estate/card-real-estate/card-real-estate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-real-estate',
+  templateUrl: './card-real-estate.component.html',
+  styleUrls: ['./card-real-estate.component.css']
+})
+export class CardRealEstateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
