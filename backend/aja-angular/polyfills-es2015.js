fbc68b1c55376fbfb4d68c1c99b5768021d11d9b
eridtroqe <erid.troqe@gmail.com>
2020-04-20 19:10:23 +0200

    secure sevannah project for deployment

--- backend/aja-angular/polyfills-es2015.js
+++ backend/aja-angular/polyfills-es2015.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\user\Desktop\Aja real estate\ajanode\aja-node\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\user\Desktop\Aja real estate\ajanode\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
