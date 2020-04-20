a4ec8287467e915ff652a1694abffb28945cd0a0
EridTroqe <etroqe@ikons-see.com>
2020-04-20 19:10:23 +0200

    secure sevannah project for deployment

--- backend/aja-angular/polyfills-es5.js
+++ backend/aja-angular/polyfills-es5.js
@@ -19078,7 +19078,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
   /***/
   function _(module, exports, __webpack_require__) {
     __webpack_require__(
-    /*! C:\Users\user\Desktop\Aja real estate\ajanode\aja-node\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */
+    /*! C:\Users\user\Desktop\Aja real estate\ajanode\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
 
     __webpack_require__(
@@ -19086,7 +19086,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     "./node_modules/zone.js/dist/zone-legacy.js");
 
     module.exports = __webpack_require__(
-    /*! C:\Users\user\Desktop\Aja real estate\ajanode\aja-node\src\polyfills.ts */
+    /*! C:\Users\user\Desktop\Aja real estate\ajanode\src\polyfills.ts */
     "./src/polyfills.ts");
     /***/
   }
